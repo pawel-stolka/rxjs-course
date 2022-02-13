70677dc5e210390cd38b166a203ca586adf539df
FP\pstolka <pstolka@future-processing.com>
2022-02-13 06:29:41 +0100

    map works

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,15 +1,28 @@
-import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { noop } from 'rxjs';
+import { createHttpObservable } from "../common/util";
+import { Component, OnInit } from '@angular/core';
+import { map } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
   templateUrl: './about.component.html',
-  styleUrls: ['./about.component.css']
+  styleUrls: ['./about.component.css'],
 })
 export class AboutComponent implements OnInit {
-
-  constructor() { }
+  constructor() {}
 
   ngOnInit() {
-  }
+    const http$ = createHttpObservable('/api/courses');
 
+    const courses$ = http$.pipe(
+      map(x => Object.values(x["payload"]))
+    )
+
+    courses$.subscribe(
+      (courses) => console.log(courses),
+      noop,
+      () => console.log('complete')
+    );
+  }
 }
+
