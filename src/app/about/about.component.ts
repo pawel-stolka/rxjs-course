2bd7af4b566818a7ddf30e0388dcb5428f84b3c5
FP\pstolka <pstolka@future-processing.com>
2022-02-13 08:19:46 +0100

    concatanation 3 streams$ when each completes

--- src/app/about/about.component.ts
+++ src/app/about/about.component.ts
@@ -1,7 +1,7 @@
-import { noop } from 'rxjs';
+import { concat, noop, of } from 'rxjs';
 import { createHttpObservable } from "../common/util";
 import { Component, OnInit } from '@angular/core';
-import { map } from 'rxjs/operators';
+import { map, tap } from 'rxjs/operators';
 
 @Component({
   selector: 'about',
@@ -12,7 +12,15 @@ export class AboutComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {
-    
+    const source1$ = of(1, 2, 3);
+    const source2$ = of(4, 5, 6);
+    const source3$ = of(7, 8, 9);
+
+    const result$ = concat(source1$, source2$, source3$);
+
+    result$
+    .pipe(tap(x => console.log('result', x)))
+    .subscribe();
   }
 }
 
