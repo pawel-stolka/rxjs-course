b24a646863f1e3755491b718c2e41c2669d3b031
FP\pstolka <pstolka@future-processing.com>
2022-02-14 06:01:43 +0100

    switchMap search & all streams

--- src/app/course/course.component.ts
+++ src/app/course/course.component.ts
@@ -30,6 +30,10 @@ import { createHttpObservable } from '../common/util';
   styleUrls: ['./course.component.css'],
 })
 export class CourseComponent implements OnInit, AfterViewInit {
+  courseId: string;
+  private courseUrl: string;
+  private lessonsUrl: string;
+
   course$: Observable<Course>;
   lessons$: Observable<Lesson[]>;
 
@@ -38,24 +42,31 @@ export class CourseComponent implements OnInit, AfterViewInit {
   constructor(private route: ActivatedRoute) {}
 
   ngOnInit() {
-    const courseId = this.route.snapshot.params['id'];
+    this.courseId = this.route.snapshot.params['id'];
 
-    const courseUrl = `/api/courses/${courseId}`;
-    const lessonsUrl = `/api/lessons?courseId=${courseId}&pageSize=100`;
+    this.courseUrl = `/api/courses/${this.courseId}`;
 
-    this.course$ = createHttpObservable(courseUrl);
-    this.lessons$ = createHttpObservable(lessonsUrl).pipe(
-      map((res) => res['payload'])
-    );
+    this.course$ = createHttpObservable(this.courseUrl);
   }
 
   ngAfterViewInit() {
-    fromEvent<any>(this.input.nativeElement, 'keyup')
-    .pipe(
-      map(event => event.target.value),
+    const searchLessons$ = fromEvent<any>(
+      this.input.nativeElement,
+      'keyup'
+    ).pipe(
+      map((event) => event.target.value),
       debounceTime(400),
-      distinctUntilChanged() // no duplicates
-    )
-    .subscribe(console.log)
+      distinctUntilChanged(), // no duplicates
+      switchMap((search) => this.loadLessons(search))
+    );
+
+    const initialLessons$ = this.loadLessons();
+
+    this.lessons$ = concat(initialLessons$, searchLessons$)
+  }
+
+  loadLessons(search = ''): Observable<Lesson[]> {
+    const lessonsUrl = `/api/lessons?courseId=${this.courseId}&pageSize=100&filter=${search}`;
+    return createHttpObservable(lessonsUrl).pipe(map((res) => res['payload']));
   }
 }
